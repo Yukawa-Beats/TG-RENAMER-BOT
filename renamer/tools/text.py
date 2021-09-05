5518e94c489c6facd4757be81ddab667620db455
Yukawa Beats <89127713+Yukawa-Beats@users.noreply.github.com>
2021-09-05 12:10:33 +0530

    Create text.py

--- renamer/tools/text.py
+++ renamer/tools/text.py
@@ -3,7 +3,7 @@ from pyrogram.emoji import *
 class TEXT:
     DOWNLOAD_START = f"Downloading Starts soon {SLEEPING_FACE}"
     UPLOAD_START = f"Upload Starting Soon {SLEEPING_FACE}"
-    UPLOAD_SUCESS = f"Thanks for using [me](https://github.com/Ns-AnoNymouS/TG-RENAMER-BOT)"
+    UPLOAD_SUCESS = f"Thanks for using [ME](https://instagram/yukawa_beats)"
     BANNED_USER_TEXT = f"Hey bro, you are **banned** from using me {FACE_WITH_TEARS_OF_JOY}."
     NOT_LOGGED_TEXT = f"This bot was only for private use {LOCKED_WITH_KEY}. If you want to use this bot you need to send me correct password in the format `/login bot_password`"
     SAVED_CUSTOM_THUMBNAIL = f"Thumbnail Saved Permanently {NOTEBOOK_WITH_DECORATIVE_COVER}"
@@ -22,11 +22,11 @@ class TEXT:
 
 ** Developer:** {bot_owner}
 
-** Channel:** [NS BOT UPDATES](https://t.me/Ns_bot_updates)
+** Channel:** [YB UPDATES](https://t.me/ybdemochannel)
 
-** Group:** [Ns BOT SUPPORT](https://t.me/Ns_Bot_supporters)
+** Instagram :** [YUKAWA BEATS](https://instagram.com/yukawa_beats)
 
-** Source Code:** [Press Me](https://github.com/Ns-AnoNymouS/TG-RENAMER-BOT)
+** Source Code:** [Press Me](https://github.com/Yukawa-Beats/TG-RENAMER-BOT)
 """
 
     HELP_USER = """**Follow Below Steps:**
@@ -41,7 +41,7 @@ For source code check about
 
     START_TEXT = """Hi {user_mention},
 
-I am a telegram renamer with permanent thumbnail.
+I Am A Telegram Renamer With Permanent Thumbnail Support.
 
 𝖥𝗈𝗋 𝖬𝗈𝗋𝖾 𝖣𝖾𝗍𝖺𝗂𝗅𝗌 𝖲𝖾𝖾 𝖧𝖾𝗅𝗉.
 
@@ -57,6 +57,6 @@ of 20rs, 30rs, 50rs, 70rs, 100rs, 200rs
  
 __--Payment Methods:--__
  
-GooglePay / Paytm / PhonPay / paypal / Net Banking
+GooglePay / Paytm / PhonPay 
  
-**For Donate:** message me @MaxxRiderz"""
+**For Donate:** message me @chekuthan_0405"""
